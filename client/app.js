0ef3a3f418d69e8f7cd5e4e7bbc08c027b513c28
Adam Thometz <adamthometz@gmail.com>
2023-09-16 17:10:00 -0400

    made index.html for pages

--- client/app.js
+++
@@ -1,9 +0,0 @@
-let currPitch = NOTE_DISPLAY_OPTIONS[0];
-let soundEnabled = false;
-
-PAPER.addEventListener("click", () => {
-  soundEnabled = !soundEnabled;
-  IS_ON_DISPLAY.textContent = `Sound: ${soundEnabled ? "On" : "Off"}`;
-});
-
-draw();
